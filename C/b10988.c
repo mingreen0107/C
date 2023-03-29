f766bf02eb5363586b32528747846b8a41320eec
mingreen0107 <71gnos@naver.com>
2023-03-29 22:58:46 +0900

    b

--- C/b10988.c
+++ C/b10988.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <string.h>
+//팰린드롬인지 확인하기
 
 int main() {
 
-	char arr[100] = { 0 };
-	int len;
+	char inputE[100] = { 0 };
+	int len, lens, cnt = 0;
 	int i;
 
-	scanf_s("%s", arr);
+	scanf("%s", inputE);
 
-	len = strlen(arr);
+	len = strlen(inputE);
+	lens = len - 1;
 
 	for (i = 0; i < len / 2; i++)
 	{
-		arr[]
+		if (inputE[i] == inputE[lens])
+		{
+			lens--;
+			cnt++;
+		}
+		else
+		{
+			break;
+		}
 	}
-
-
-
-
+	if (cnt == len / 2)
+	{
+		printf("1");
+	}
+	else
+	{
+		printf("0");
+	}
 	return 0;
 }
