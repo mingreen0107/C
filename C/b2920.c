fe025f02697b6d4fa3bfbcf7a074dfd136906b14
mingreen0107 <71gnos@naver.com>
2023-04-01 21:10:58 +0900

    b

--- C/b2920.c
+++ C/b2920.c
@@ -2,21 +2,25 @@
 
 int main() {
 
-	int inputN[8] = { 0 };
+	int inputN[8] = { 0, };
+	int check = 0;
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-		scanf("%d ", &inputN[i]);
+		scanf("%d", &inputN[i]);
 	}
 	for (i = 0; i < 8; i++)
 	{
-		if(inputN[i]==i)
+		if (inputN[i] == i + 1)
+			check += 1;
+		else if (inputN[i] == 8 - i)
+			check -= 1;
+		else
+			check += 100;
 	}
-
-
-
-
-
+	if (check == 8) printf("ascending");
+	else if (check == -8) printf("descending");
+	else printf("mixed");
 	return 0;
 }
