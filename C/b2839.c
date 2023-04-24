48e6dbe2322f68161d6d6091c0ba35baa0e116bc
mingreen0107 <71gnos@naver.com>
2023-04-24 21:02:48 +0900

    b

--- C/b2839.c
+++ C/b2839.c
@@ -1,7 +1,15 @@
 // 주어진 숫자에서 3, 5를 나누고 몫의 합이 제일 작은거 구하기
 #include <stdio.h>
 
-int check_bag(int N) {
+int check3_bag(int N) {
+	if (N % 3 != 0)
+		return -1;
+	else
+		return N / 3;
+}
+
+int check5_bag(int N) {
+	int share, rest, bag;
 
 	if (N < 5)
 		if (N % 3 != 0)
@@ -9,21 +17,37 @@ int check_bag(int N) {
 		else
 			return 1;
 	else {
-
+		if (N % 5 != 0) {
+			bag = N / 5;
+			rest = N % 5;
+			if (rest == 3)
+				return bag + 1;
+			else
+				return -1;
+		}
+		else
+			bag = N / 5;
+		return bag;
 	}
-
-	
-	
 }
 
 int main() {
 	
-	int N, bag;
+	int N, bag1, bag2;
 
 	scanf("%d", &N);
 
-	bag = check_bag(N);
-	printf("%d", bag);
+	bag1 = check3_bag(N);
+	bag2 = check5_bag(N);
+	if (bag1 <= bag2)
+		printf("%d", bag1);
+	else {
+		if (bag2 == -1)
+			printf("%d", bag1);
+		else
+			printf("%d", bag2);
+	}
+		
 
 	return 0;
 }
