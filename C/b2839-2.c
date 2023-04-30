e05983588de516516a0869de80c2f0e7ed11ae02
mingreen0107 <71gnos@naver.com>
2023-04-30 20:27:30 +0900

    b

---
+++ C/b2839-2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int check3_bag(int N) {
+	int share = 0;
+
+	for (int i = 0; ; i++) {
+		for (int j = 0; ; j++) {
+			if (5 * i + 3 * j == N)
+				share = i + j;
+		}
+	}
+}
+int check5_bag(int N) {
+	
+}
+
+int main() {
+
+	int N;
+
+	scanf("%d", &N);
+
+
+	return 0;
+}
