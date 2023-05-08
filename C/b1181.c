809367e1f752c5ff50b8ee1ac97ef213a6c7f584
mingreen0107 <71gnos@naver.com>
2023-05-08 19:12:33 +0900

    b

--- C/b1181.c
+++ C/b1181.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-	
-	int N;
-	char arr[50] = { 0 };
+int compare(const void* arr1, const void* arr2)
+{
+	if (strlen((const char*)arr1) > strlen((const char*)arr2))
+		return 1;
+	else if (strlen((const char*)arr1) < strlen((const char*)arr2))
+		return -1;
+
+	else return strcmp((char*)arr1, (char*)arr2);
+}
+int main(void)
+{
+	int N, length = 51;
 	int i;
-
+	char arr[20000][51] = { 0 };
 	scanf("%d", &N);
 
 	for (i = 0; i < N; i++)
-	{
-		scanf("%s", arr);
+		scanf("%s", arr[i]);
+
+	qsort(arr, N, sizeof(arr[0]), compare);
+
+	for (i = 0; i < N; i++) {
+		if (strcmp(arr[i], arr[i + 1]) != 0 || i == N - 1)
+			printf("%s\n", arr[i]);
 	}
-
-
-
 	return 0;
 }
