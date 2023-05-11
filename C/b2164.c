34c4171da6a744e5defca6b31a7830c0b9f3484a
mingreen0107 <71gnos@naver.com>
2023-05-11 16:26:51 +0900

    b

---
+++ C/b2164.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+123456
+23456
+34562
+4562
+5624
+624
+246
+46
+64
+4
+*/
+void alone(int* arr, int N) {
+	int i, front;
+
+	for (i = 0; i < N; i++)
+		arr[i] = i + 1;
+
+	while (N != 1) {
+		N -= 1;
+		for (i = 0; i < N; i++) {
+			arr[i] = arr[i + 1];
+			front = arr[0];
+		}
+		for (i = 0; i < N; i++) {
+			if (i == N - 1)
+				arr[i] = front;
+			else
+				arr[i] = arr[i + 1];
+		}
+	}
+	printf("%d", arr[0]);
+}
+
+int main() {
+
+	int N;
+
+	scanf("%d", &N);
+
+	int* arr = (int*)malloc(sizeof(int)*N);
+
+	alone(arr, N);
+
+	free(arr);
+
+	return 0;
+}
